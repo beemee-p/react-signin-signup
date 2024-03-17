ebddc5d22be3cfd1d4a38f477104e95339d07a58
Yuna Park <una@stunning.kr>
2024-03-17 17:16:46 +0900

    add: signin inputs

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,13 @@
 import React, { ReactElement } from "react";
+import { Link } from "react-router-dom";
 
 const Main = (): ReactElement => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <Link to="/signin">로그인</Link>
+      <Link to="/signup">회원가입</Link>
+    </div>
+  );
 };
 
 export default Main;
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,40 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
+import { signIn } from "../apis/signIn";
 
 const SignIn = (): ReactElement => {
-  return <div>Signin</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSignIn() {
+    console.log(email + " " + password);
+    await signIn(email, password);
+  }
+
+  return (
+    <div>
+      <article>
+        <span>이메일</span>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="이메일"
+        />
+      </article>
+
+      <article>
+        <span>비밀번호</span>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="비밀번호"
+        />
+      </article>
+
+      <button onClick={handleSignIn}>로그인</button>
+    </div>
+  );
 };
 
 export default SignIn;
